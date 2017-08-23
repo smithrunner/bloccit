052cfb6d4d054577e8aceb53623f94a69845b5a6
Sam Smith <smithrunner@live.com>
2017-08-23 15:27:16 +0000

    assignment-18 remvoing class method for censor data

--- lib/censor_data.rb
+++ lib/censor_data.rb
@@ -1,13 +1,13 @@
 module CensorData
   def self.censor(posts)
     posts.each_with_index do |post, index|
-      self.if_first_or_increment_of_5_post(index) ? post.title = "SPAM" : next
+      if_first_or_increment_of_5_post(index) ? post.title = "SPAM" : next
     end
     
     return posts
   end
   
-  def self.if_first_or_increment_of_5_post(index)
+  def if_first_or_increment_of_5_post(index)
     first_or_fith_boolean = false
     
     if index == 0
