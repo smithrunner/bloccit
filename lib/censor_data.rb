509265547805478c24a83143b93fe5c1cd9a2c72
Sam Smith <smithrunner@live.com>
2017-08-23 02:10:11 +0000

    assignment 18 update update

--- lib/censor_data.rb
+++ lib/censor_data.rb
@@ -1,15 +1,22 @@
 module CensorData
   def self.censor(posts)
-    self.spam(posts[0])
     posts.each_with_index do |post, index|
-      (index+1) % 5 == 0 ? self.spam(post) : next
+      self.if_first_or_increment_of_5_post(index) ? post.title = "SPAM" : next
     end
     
     return posts
   end
   
-  def self.spam(post)
-    post.title = "SPAM"
+  def self.if_first_or_increment_of_5_post(index)
+    first_or_fith_boolean = false
+    
+    if index == 0
+      return first_or_fith_boolean = true
+    elsif (index+1) % 5 == 0
+      return first_or_fith_boolean = true
+    end
+  
+    return first_or_fith_boolean
   end
   
 end
