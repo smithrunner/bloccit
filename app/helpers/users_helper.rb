e6d46190ec892642f614ae0422d70ed82ce1149e
Sam Smith <smithrunner@live.com>
2017-09-01 16:53:54 +0000

    assignment-32 complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def any_posts_yet?
+    @user.posts.count > 0 ? true : false
+  end
+  
+  def any_comments_yet?
+    @user.comments.count > 0 ? true : false
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Comment, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
+  let(:comment) { create(:comment) }
   
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
@@ -12,11 +12,6 @@ RSpec.describe Comment, type: :model do
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_length_of(:body).is_at_least(5) }
   
-  describe "attributes" do
-    it "has a body attribute" do
-      expect(comment).to have_attributes(body: "Comment Body")
-    end
-  end
   
   describe "after_create" do
     before do
